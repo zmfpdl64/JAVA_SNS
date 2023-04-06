0075d7c37dc3a40e9bbb235da09e6cb083ff3952
woojin <zmfpdl64@naver.com>
2023-04-06 20:45:47 +0900

    chore(Login): 로그인 할 때 예외 상황 추가
    1. 아이디 존재하지 않을 때
    2. 아이디 일치 비밀번호 불일치

--- src/main/java/personal/sns/exception/Errorcode.java
+++ src/main/java/personal/sns/exception/Errorcode.java
@@ -9,8 +9,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum Errorcode {
-    DUPLICATE_USERNAME(HttpStatus.UNAUTHORIZED, "회원 아이디가 중복 되었습니다."),
-    NOT_MATCH_AUTH(HttpStatus.INTERNAL_SERVER_ERROR, "회원 아이디 비밀번호가 일치하지 않습니다"),
+    DUPLICATE_USERNAME(HttpStatus.NOT_FOUND, "회원 아이디가 중복 되었습니다."),
+    NOT_EXISTS_USERNAME(HttpStatus.NOT_FOUND, "회원 아이디가 존재하지 않습니다"),
+    NOT_MATCH_PASSWORD(HttpStatus.NOT_FOUND, "회원 비밀번호가 일치하지 않습니다"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예상치 못한 에러가 발생했습니다");
 
     private final HttpStatus status;
