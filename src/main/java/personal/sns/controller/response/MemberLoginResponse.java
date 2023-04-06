91bc44a65bcc3f4dabf93881f4f18261073ad5f1
woojin <zmfpdl64@naver.com>
2023-04-06 20:44:33 +0900

    feat(Login): 로그인 토큰 적용

--- src/main/java/personal/sns/controller/response/MemberLoginResponse.java
+++ src/main/java/personal/sns/controller/response/MemberLoginResponse.java
@@ -2,11 +2,9 @@ package personal.sns.controller.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import personal.sns.domain.Member;
 
 @Getter
 @AllArgsConstructor
-public class MemberLoginResposne {
-    private String username;
-    private String password;
+public class MemberLoginResponse {
+    private String token;
 }
--- src/main/java/personal/sns/service/MemberService.java
+++ src/main/java/personal/sns/service/MemberService.java
@@ -1,6 +1,7 @@
 package personal.sns.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,6 +10,7 @@ import personal.sns.domain.entity.MemberEntity;
 import personal.sns.exception.Errorcode;
 import personal.sns.exception.SnsException;
 import personal.sns.repository.MemberRepository;
+import personal.sns.util.JwtTokenUtils;
 
 @Transactional
 @RequiredArgsConstructor
@@ -17,9 +19,15 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder encoder;
 
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.token.expired-time-ms}")
+    private Long expiredTimeMs;
+
     public Member join(String username, String password) {
         memberRepository.findByName(username).ifPresent(findmember -> {
-            throw new SnsException(Errorcode.DUPLICATE_USERNAME, String.format("username is %s", username));
+            throw new SnsException(Errorcode.DUPLICATE_USERNAME, String.format("유저 아이디: %s", username));
         });
 
         MemberEntity member = memberRepository.save(MemberEntity.of(username, encoder.encode(password)));
@@ -27,12 +35,15 @@ public class MemberService {
         return Member.fromEntity(member);
     }
 
-    public Member login(String username, String password) {
-        MemberEntity member = memberRepository.findByName(username).orElseThrow(() -> new SnsException(Errorcode.NOT_MATCH_AUTH));
-        if (!member.getPassword().equals(password)){
-            throw new SnsException(Errorcode.NOT_MATCH_AUTH);
+    public String login(String username, String password) {
+        // 회원가입 여부 체크
+        MemberEntity member = memberRepository.findByName(username).orElseThrow(() -> new SnsException(Errorcode.NOT_EXISTS_USERNAME, String.format("%s를 찾지 못했습니다.", username)));
+        // 비밀번호 체크
+        if (!encoder.matches(password, member.getPassword())){
+            throw new SnsException(Errorcode.NOT_MATCH_PASSWORD);
         }
-        return Member.fromEntity(member);
+        // 토큰 생성
+        return JwtTokenUtils.generateToken(username, secretKey, expiredTimeMs);
     }
 
 
