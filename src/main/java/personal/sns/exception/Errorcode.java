06b462577307ecdeef8d8a6a426fdc3adbfac9a0
woojin <zmfpdl64@naver.com>
2023-04-06 19:43:43 +0900

    feat(Login): 로그인할 때 발생할 예외처리 작성
    1. 아이디가 존재하지 않을 때
    2. 비밀번호가 존재하지 않을 때
    3. DB나 예상치 못한 곳에서 RuntimeException이 발생할 때

--- src/main/java/personal/sns/exception/Errorcode.java
+++ src/main/java/personal/sns/exception/Errorcode.java
@@ -1,4 +1,4 @@
-package personal.sns.exception.exception;
+package personal.sns.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,8 +9,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum Errorcode {
-    DUPLICATE_USERNAME(HttpStatus.UNAUTHORIZED, "회원 아이디가 중복 되었습니다.");
-
+    DUPLICATE_USERNAME(HttpStatus.UNAUTHORIZED, "회원 아이디가 중복 되었습니다."),
+    NOT_MATCH_AUTH(HttpStatus.INTERNAL_SERVER_ERROR, "회원 아이디 비밀번호가 일치하지 않습니다"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예상치 못한 에러가 발생했습니다");
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/personal/sns/service/MemberService.java
+++ src/main/java/personal/sns/service/MemberService.java
@@ -1,12 +1,13 @@
 package personal.sns.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import personal.sns.domain.Member;
 import personal.sns.domain.entity.MemberEntity;
-import personal.sns.exception.exception.Errorcode;
-import personal.sns.exception.exception.SnsException;
+import personal.sns.exception.Errorcode;
+import personal.sns.exception.SnsException;
 import personal.sns.repository.MemberRepository;
 
 @Transactional
@@ -14,14 +15,23 @@ import personal.sns.repository.MemberRepository;
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder encoder;
 
     public Member join(String username, String password) {
         memberRepository.findByName(username).ifPresent(findmember -> {
             throw new SnsException(Errorcode.DUPLICATE_USERNAME, String.format("username is %s", username));
         });
 
-        MemberEntity member = memberRepository.save(MemberEntity.of(username, password));
+        MemberEntity member = memberRepository.save(MemberEntity.of(username, encoder.encode(password)));
 
+        return Member.fromEntity(member);
+    }
+
+    public Member login(String username, String password) {
+        MemberEntity member = memberRepository.findByName(username).orElseThrow(() -> new SnsException(Errorcode.NOT_MATCH_AUTH));
+        if (!member.getPassword().equals(password)){
+            throw new SnsException(Errorcode.NOT_MATCH_AUTH);
+        }
         return Member.fromEntity(member);
     }
 
