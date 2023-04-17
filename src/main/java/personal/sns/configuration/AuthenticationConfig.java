cc72ff39e6f153ed512c5584ba26fc988af85485
leewoojin <zmfpdl64@naver.com>
2023-04-17 14:25:22 +0900

    feat(modifypost): 예외 상황 추가로 처리 및 경로 설정 추가
    AuthenticationConfig:
    api로 시작하는 모든 작업은 인증을 걸쳐야 하며
    그 외의 것은 허용한다.
    
    Errorcod 조건 추가
    1. 회원 권한 x
    2. 게시글 존재 x

--- src/main/java/personal/sns/configuration/AuthenticationConfig.java
+++ src/main/java/personal/sns/configuration/AuthenticationConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import personal.sns.configuration.filter.JwtTokenFilter;
@@ -29,7 +30,8 @@ public class AuthenticationConfig{
                 .authorizeHttpRequests(authorize ->
                     authorize
                             .requestMatchers("/api/v1/user/**").permitAll()
-                            .anyRequest().authenticated()
+                            .requestMatchers("/api/**").authenticated()
+                            .anyRequest().permitAll()
                 )
                 .exceptionHandling()
                 .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
--- src/main/java/personal/sns/exception/GlobalControllerAdvicer.java
+++ src/main/java/personal/sns/exception/GlobalControllerAdvicer.java
@@ -13,12 +13,14 @@ public class GlobalControllerAdvicer {
 
     @ExceptionHandler(SnsException.class)
     public ResponseEntity<?> applicationHandler(SnsException e){
+        log.error("Error: {}", e.getMessage());
         return ResponseEntity.status(e.getErrorcode().getStatus())
                 .body(Response.error(e.getErrorcode().name()));
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<?> runtimeHandler() {
+    public ResponseEntity<?> runtimeHandler(RuntimeException e) {
+        log.error("Error: {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(Response.error(Errorcode.INTERNAL_SERVER_ERROR.name()));
     }
