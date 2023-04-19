d9dfd5260669eec7944d0e3fe99ee40f45830432
leewoojin <zmfpdl64@naver.com>
2023-04-19 12:32:00 +0900

    feat(getMyPost): 내 게시글 가져오기 @Param 추가

--- src/main/java/personal/sns/repository/PostEntityRepository.java
+++ src/main/java/personal/sns/repository/PostEntityRepository.java
@@ -4,11 +4,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import personal.sns.domain.entity.PostEntity;
 
 import java.util.Optional;
 
 public interface PostEntityRepository extends JpaRepository<PostEntity, Integer> {
     @Query("select p from PostEntity p where p.member.name = :username")
-    Page<PostEntity> findByMemberName(Pageable pageable, String username);
+    Page<PostEntity> findByMemberName(Pageable pageable,@Param("username") String username);
 }
