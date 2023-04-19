cec16ff66f0a337597e4ef5f82f784590547f0a6
leewoojin <zmfpdl64@naver.com>
2023-04-19 12:09:51 +0900

    feat(getMyPost): 내 게시글 가져오기 기능 구현

--- src/main/java/personal/sns/repository/PostEntityRepository.java
+++ src/main/java/personal/sns/repository/PostEntityRepository.java
@@ -1,10 +1,14 @@
 package personal.sns.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import personal.sns.domain.entity.PostEntity;
 
 import java.util.Optional;
 
 public interface PostEntityRepository extends JpaRepository<PostEntity, Integer> {
-
+    @Query("select p from PostEntity p where p.member.name = :username")
+    Page<PostEntity> findByMemberName(Pageable pageable, String username);
 }
