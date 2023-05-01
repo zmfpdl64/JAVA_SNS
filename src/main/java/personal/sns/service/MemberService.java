0eb45e1af4f3fafe1078f4d54f59c1a37cef9ddd
leewoojin <zmfpdl64@naver.com>
2023-05-01 11:25:47 +0900

    feat(Redis): Redis의 Member 클래스 캐싱 적용
    토큰을 검사할 때 항상 loadMemberByMemberName을 호출하여
    DB를 검사해야하는 현상이 있었다.
    그래서 중간 캐싱서버를 두어 DB의 부하를 덜어주었다.

--- src/main/java/personal/sns/service/MemberService.java
+++ src/main/java/personal/sns/service/MemberService.java
@@ -13,6 +13,7 @@ import personal.sns.domain.entity.MemberEntity;
 import personal.sns.exception.Errorcode;
 import personal.sns.exception.SnsException;
 import personal.sns.repository.AlarmEntityRepository;
+import personal.sns.repository.MemberCacheRepository;
 import personal.sns.repository.MemberRepository;
 import personal.sns.util.JwtTokenUtils;
 
@@ -22,6 +23,7 @@ import personal.sns.util.JwtTokenUtils;
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final MemberCacheRepository cacheRepository;
     private final AlarmEntityRepository alarmRepository;
     private final BCryptPasswordEncoder encoder;
 
@@ -32,8 +34,12 @@ public class MemberService {
     private Long expiredTimeMs;
 
     public Member loadMemberByMemberName(String userName) {
-        return memberRepository.findByName(userName).map(Member::fromEntity).orElseThrow(() ->
-                new SnsException(Errorcode.NOT_EXISTS_USERNAME, String.format("%s not founded", userName)));
+        return cacheRepository.getMember(userName)
+                .orElseGet(
+                        () -> memberRepository.findByName(userName).map(Member::fromEntity).orElseThrow(() ->
+                                        new SnsException(Errorcode.NOT_EXISTS_USERNAME, String.format("%s not founded", userName)))
+                        );
+
     }
 
     public Member join(String username, String password) {
@@ -48,11 +54,15 @@ public class MemberService {
 
     public String login(String username, String password) {
         // 회원가입 여부 체크
-        MemberEntity member = memberRepository.findByName(username).orElseThrow(() -> new SnsException(Errorcode.NOT_EXISTS_USERNAME, String.format("%s를 찾지 못했습니다.", username)));
+        Member member = loadMemberByMemberName(username);
+
         // 비밀번호 체크
         if (!encoder.matches(password, member.getPassword())){
             throw new SnsException(Errorcode.NOT_MATCH_PASSWORD);
         }
+        // 멤버 캐싱
+        cacheRepository.setMember(member);
+
         // 토큰 생성
         return JwtTokenUtils.generateToken(username, secretKey, expiredTimeMs);
     }
